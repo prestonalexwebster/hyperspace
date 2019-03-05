7330f6b559e470ea8b8dc48c49ce418a3c4bf1f2
ASAnokhin <booolbash@gmail,.com>
2019-03-05 19:32:49 +0300

    Adding backend

---
+++ src/main/java/fd/hyperspace/HyperspaceApplication.java
@@ -0,0 +1,13 @@
+package fd.hyperspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperspaceApplication.class, args);
+    }
+
+}
